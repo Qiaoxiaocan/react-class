a24b7b9d49308421cbe1403e19d2620dcca22546
Qiaoxiaocan <1293941868@qq.com>
2020-10-17 18:39:55 +0800

    第二次作业

---
+++ build02/precache-manifest.1cee964797d9c000c215eca2bb065aa3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e700926025bd3d08278431382862d8b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4284b1c2acd1bbe83111",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "4a9cbae4bed289f78571",
+    "url": "/static/css/main.11203ba7.chunk.css"
+  },
+  {
+    "revision": "4284b1c2acd1bbe83111",
+    "url": "/static/js/2.56b82506.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.56b82506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a9cbae4bed289f78571",
+    "url": "/static/js/main.a56f75e3.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
