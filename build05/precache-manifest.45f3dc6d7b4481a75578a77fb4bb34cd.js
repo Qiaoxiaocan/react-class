d3ea34d111f2fe6240ba8cafdbabdb0d00cc350c
Qiaoxiaocan <1293941868@qq.com>
2020-11-10 16:24:47 +0800

    05

---
+++ build05/precache-manifest.45f3dc6d7b4481a75578a77fb4bb34cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a64756c552cc92a83d15631231574ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "785cb27f04f71dbb6b3d",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "90113e926cd8a82014d9",
+    "url": "/static/css/main.89444947.chunk.css"
+  },
+  {
+    "revision": "785cb27f04f71dbb6b3d",
+    "url": "/static/js/2.5d816202.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.5d816202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90113e926cd8a82014d9",
+    "url": "/static/js/main.3697b0b4.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
